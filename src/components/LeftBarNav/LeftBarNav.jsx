e634f1dfd74fefd24a8c79d28ea8ca81d96956d8
Ahindra D <official.ahindra@gmail.com>
2022-09-05 17:00:40 +0530

    sidebar done

--- src/components/LeftBarNav/LeftBarNav.jsx
+++ src/components/LeftBarNav/LeftBarNav.jsx
@@ -9,41 +9,81 @@ import playlist from "./playlist.svg"
 import github from "./github.svg"
 import { Link, useNavigate } from 'react-router-dom';
 
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import userContext from "../../Context/UserContext";
 
 export default function Sidebar() {
   const { userData, token } = useContext(userContext);
+  const goTo = useNavigate();
+
+  const navMap = {
+    "prof": "/home",
+    "artst": "/home/artists",
+    "trac": "/home/tracks",
+    "rcnt": "/home/activity",
+    "plist": "/home/playlists",
+  }
+
+  let [prof, setProf] = useState("");
+  let [artst, setArtst] = useState("");
+  let [trac, setTrac] = useState("");
+  let [rcnt, setRcnt] = useState("");
+  let [plist, setPlist] = useState("");
+
+  function handleClick(arg) {
+    goTo(navMap[arg]);
+    setProf("");
+    setArtst("");
+    setTrac("");
+    setRcnt("");
+    setPlist("");
+
+    if (arg === "prof") {
+      setProf("active");
+    }
+    else if (arg === "artst") {
+      setArtst("active");
+    }
+    else if (arg === "trac") {
+      setTrac("active");
+    }
+    else if (arg === "rcnt") {
+      setRcnt("active");
+    }
+    else if (arg === "plist") {
+      setPlist("active");
+    }
+  }
 
   return (
     <main className="bar-cont">
-      <div className="logo">
+      <div className="logo" onClick={() => handleClick("prof")}>
         <img src={spotify} alt="spotify-logo" />
       </div>
 
       <section className="navs">
 
-        <div className="nav prof">
+        <div className={`nav ${prof}`} onClick={() => handleClick("prof")}>
           <img src={profile} alt="profile-logo" />
           <p>Profile</p>
         </div>
 
-        <div className="nav art">
+        <div className={`nav ${artst}`} onClick={() => handleClick("artst")}>
           <img src={mic} alt="artists-logo" />
           <p>Top Artists</p>
         </div>
 
-        <div className="nav trac">
+        <div className={`nav ${trac}`} onClick={() => handleClick("trac")}>
           <img src={music} alt="tracks-logo" />
           <p>Top Tracks</p>
         </div>
 
-        <div className="nav rect">
+        <div className={`nav ${rcnt}`} onClick={() => handleClick("rcnt")}>
           <img src={recents} alt="recents-logo" />
           <p>Recents</p>
         </div>
 
-        <div className="nav plist">
+        <div className={`nav ${plist}`} onClick={() => handleClick("plist")}>
           <img src={playlist} alt="playlists-logo" />
           <p>Playlists</p>
         </div>
