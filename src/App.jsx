fb30eb9420925c9c3204fe245babef0860d9de97
Ahindra D <official.ahindra@gmail.com>
2022-09-05 12:54:03 +0530

    user-data

--- src/App.jsx
+++ src/App.jsx
@@ -45,26 +45,6 @@ function App() {
   //   return user;
   // };
 
-  // useEffect(() => {
-  //   const hash = window.location.hash;
-  //   let token = window.localStorage.getItem("token");
-
-  //   if (!token && hash) {
-  //     token = hash
-  //       .substring(1)
-  //       .split("&")
-  //       .find((elem) => elem.startsWith("access_token"))
-  //       .split("=")[1];
-
-  //     window.location.hash = "";
-  //     window.localStorage.setItem("token", token);
-  //   }
-
-  //   setToken(() => token);
-  //   //console.log(token)
-  //   user_account(token);
-  // }, []);
-
   function goToProfile() {
     //setTimeout(() => {
     navigate("/validate");
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState, useContext } from "react";
 import userContext from '../Context/UserContext';
 
 export default function HomePage() {
     const { userData, setUserData, token, setToken } = useContext(userContext);
+
+    async function user_account(token) {
+        console.log(token);
+        const user = await axios
+            .get("https://api.spotify.com/v1/me", {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            })
+            .then((response) => {
+                // Return the full details of the user.
+                //console.log(response);
+
+                //setUserData(() => response.slice);
+                //console.log(userData);
+                return response;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        console.log(user);
+        setUserData(() => user)
+    };
+
+    useEffect(() => {
+        user_account(token);
+    }, [])
+
+
+
     return (
         <div className='home'>H-O-M-E</div>
     )
