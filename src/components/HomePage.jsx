881d2700a7b8e2ccd0327c67bdf5f1c54f1cec03
Ahindra D <official.ahindra@gmail.com>
2022-09-05 15:06:54 +0530

    styled

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+import { Routes, Route, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState, useContext } from "react";
 import userContext from '../Context/UserContext';
 
