47b90894e9507f6a704d3b3b8f5ad3cbff06f300
Ahindra D <official.ahindra@gmail.com>
2022-09-05 19:23:05 +0530

    profile done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,20 +1,49 @@
 import React from 'react'
+import "./Profile.css"
+import avatar from "./avatar.svg"
 import { useContext, useState } from 'react';
 import userContext from "../../Context/UserContext";
 
 export default function Profile() {
-    const { userData, token } = useContext(userContext);
+    const { userData, token, playlists, setPlaylists } = useContext(userContext);
+
+
+
+    
     return (
-        <div className='profile-cont'>
-            <div className="profile-top">
-                <div className="avatar">
-                    <img src={userData !== null ? userData.images[0].url : ""} alt="" />
-                </div>
-                <p className="name">{userData !== null ? userData.display_name : ""}</p>
-            </div>
+        <>
+            {
+                userData === null ? "" :
+                    <div className='profile-cont'>
+                        <div className="profile-top">
+                            <div className="avatar">
+                                <img src={userData.images.length > 0 ? userData.images[0].url : avatar} alt="" />
+                            </div>
+                            <p className="name">{userData.display_name}</p>
+                            <div className="stats">
+                                <section className='stat'>
+                                    <p className="count">{userData.followers.total}</p>
+                                    <p className="type">Followers</p>
+                                </section>
+
+                                <section className='stat'>
+                                    <p className="count">{userData.followers.total}</p>
+                                    <p className="type">Following</p>
+                                </section>
+
+                                <section className='stat'>
+                                    <p className="count">{playlists.length}</p>
+                                    <p className="type">Playlists</p>
+                                </section>
+                            </div>
+
+                            <button className='logout'>Logout</button>
+                        </div>
 
 
-            <div className="profile-bottom"></div>
-        </div>
+                        <div className="profile-bottom"></div>
+                    </div>
+            }
+        </>
     )
 }
