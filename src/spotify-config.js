ce60552ffc5980edadfaf6f0a3fbbfb18ea69cd1
Ahindra D <official.ahindra@gmail.com>
2022-09-06 09:43:20 +0530

    added deployment redirect

--- src/spotify-config.js
+++ src/spotify-config.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = "0d8279df26c4479fa288fb993fc0afce";
-const REDIRECT_URI = "http://localhost:3000/validate";
+const REDIRECT_URI = "https://spotify-profile-app.netlify.app/validate";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
 const SCOPE = "ugc-image-upload user-follow-read playlist-read-private user-top-read playlist-read-collaborative user-read-recently-played";
